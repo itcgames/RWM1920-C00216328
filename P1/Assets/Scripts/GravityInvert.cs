7aecd3032482950e2f5cba2e98f1b0cfdade9afb
GregCahillC00216328 <C00216328@itcarlow.ie>
2019-11-12 12:29:24 +0000

    Added Feature 1 p1

---
+++ P1/Assets/Scripts/GravityInvert.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityInvert : MonoBehaviour {
+    public Rigidbody Rb;
+    public static double invertVal=1;
+    int rounded;
+    void OnTriggerStay(Collider other)
+    {
+        rounded = Mathf.RoundToInt((float)invertVal);
+        Rb = other.gameObject.GetComponent<Rigidbody>();
+        Rb.AddForce(-rounded*(Physics.gravity) * Rb.mass);
+        Debug.Log(invertVal);
+    }
+}
